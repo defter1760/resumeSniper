4ef2ff50913b03836cb7e526fc39e660885301e3
defter1760 <defter@gmail.com>
2015-12-10 00:06:48 -0800

    Update mySQLconnect.php

--- sniper/mySQLconnect.php
+++ sniper/mySQLconnect.php
@@ -1,6 +1,6 @@
 <?PHP
 ##connect to MySQL DB
-$link = mysql_connect('localhost', 'ian', 'maniacman669')
+$link = mysql_connect('localhost', '##########', '###################')
     or die('Could not connect: ' . mysql_error());
 #echo 'Connected successfully';
 mysql_select_db('sniper') or die('Could not select database');
